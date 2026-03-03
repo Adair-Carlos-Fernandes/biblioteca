0a5570c6bc4721aaf3b925701209a176cd9fc8c5
Adair-Carlos-Fernandes <adaircnc@hotmail.com>
2026-03-03 19:02:19 -0300

    biblioteca

---
+++ app/Http/Controllers/livrosController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controller;
+
+use Illuminate\Http\Request;
+
+class livroController extends Controller
+{
+    public function index()
+    {
+        dd('Seja bem vindo');
+    }
+}
