930d4c0344902a0cca16bd79f8e56a9785c36927
Adair-Carlos-Fernandes <adaircnc@hotmail.com>
2026-03-12 21:15:17 -0300

    biblioteca

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html long="en">
+<head>
+    <meta charset="UTF-8">
+    <meta html-equiv="X-UA-Compatible" content="IE-edge">
+    <meta nome="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteca</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,5 @@ Route::get('/', function () {
 });
 
 Route::get('/livros',function(){
-    Return view('livros');
+    return view('livros');
 });
